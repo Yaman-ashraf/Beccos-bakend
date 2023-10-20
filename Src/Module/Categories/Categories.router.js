c7410a6d7c7e8787d7489e7fcef211be72a38ec1
Yaman-ashraf <yaman472001@gmail.com>
2023-10-21 01:35:28 +0300

    n

--- Src/Module/Categories/Categories.router.js
+++ Src/Module/Categories/Categories.router.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import * as categoriesController from './Categories.controller.js';
 
 const router = Router();
-
 router.get('/', categoriesController.getCategories)
 
 export default router;
