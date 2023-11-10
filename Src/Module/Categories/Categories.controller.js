635a67eb9b30454217e713ba458d21e8bd863cf5
Yaman-ashraf <yaman472001@gmail.com>
2023-11-10 23:35:54 +0200

    delete category

--- Src/Module/Categories/Categories.controller.js
+++ Src/Module/Categories/Categories.controller.js
@@ -1,14 +1,38 @@
 import slugify from "slugify";
 import categoryModel from "../../../DB/model/Category.model.js";
+import cloudinary from "../../Services/cloudinary.js";
 
 export const createCategory = async (req, res) => {
     const name = req.body.name.toLowerCase();
 
     const slug = slugify(name);
 
-    const category = await categoryModel.create({ name, slug });
+    // return res.json(req.files);
+    const { secure_url, public_id } = await cloudinary.uploader.upload(req.files.mainImage[0].path, { //return url&id for photo
+        folder: `${process.env.APP_NAME}/categories`
+    })
+
+    const category = await categoryModel.create({ name, slug, image: { secure_url, public_id } });
+
     return res.status(201).json({ message: "SUCCESS", category });
 }
-export const getCategories = (req, res) => {
-    return res.json({ message: "cat" });
+export const getCategories = async (req, res) => {
+    const categories = await categoryModel.find();
+    return res.status(200).json({ message: "Success", categories });
+}
+export const getCategory = async (req, res) => {
+    const { id } = req.params;
+    const category = await categoryModel.findById(id);
+    return res.status(200).json({ message: "Success", category });
+
+}
+export const deleteCategory = async (req, res) => {
+    const { id } = req.params;
+    const category = await categoryModel.findById(id);
+    if (!category) {
+        return res.status(404).json({ message: "Not Found" });
+    }
+    await cloudinary.uploader.destroy(category.image.public_id);
+    await categoryModel.findByIdAndDelete(id);
+    return res.status(200).json({ message: "Success" });
 }
---
+++ Src/Services/multer.js
@@ -0,0 +1,22 @@
+import multer from 'multer';
+
+export const fileValidation = {
+    image: ['image/jpeg', 'image/png', 'image / webp'],
+    file: ['aplication/pdf'],
+}
+
+
+function fileUpload(customValidation = []) {
+
+    const storage = multer.diskStorage({});//place where i store the pic, not in my lab
+    function fileFilter(req, file, cb) {//callBack
+        if (customValidation.includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+            cb("invalid format", false);
+        }
+    }
+    const upload = multer({ fileFilter, storage });
+    return upload;
+}
+export default fileUpload;
