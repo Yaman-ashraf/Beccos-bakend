5879096a5c7d4a113e8bb20202fb411e0d9b80de
Hibaawad02 <hiba2002pl@gmail.com>
2023-12-10 20:48:00 +0200

    create products front

---
+++ front/assets/js/autho.js
@@ -0,0 +1,26 @@
+const loginClick = document.querySelector("#loginClick");
+loginClick.addEventListener("click",function(e){// to not refresh the page
+    e.preventDefault();
+    login();
+})
+
+const login = async ()=>{
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+
+    try{
+        const {data} = await axios.post(`https://beccos.onrender.com/auth/signin`,{email,password});
+
+            console.log(data);
+        if(data.message=='success'){
+            localStorage.setItem("token",data.token);
+            location.href='../../src/categories/index.html';
+        }
+    }catch(error){
+        document.querySelector(".invalid").classList.remove("invalid");
+    }
+
+
+
+
+}
--- front/src/products/products.js
+++ front/src/products/products.js
@@ -38,27 +38,27 @@ createCategoryForm?.addEventListener('submit', async function(e) {
 
 })
 
-const getCategories = async()=>{
+const getProducts = async()=>{
 
-  const {data} =await axios.get(`https://beccos.onrender.com/categories`);
-   return data.categories;
+  const {data} =await axios.get(`https://beccos.onrender.com/products`);
+   return data.products;
 
 }
-const displayCategory = async ()=>{
-const categories = await getCategories();
-console.log(categories);
+const displayProducts = async ()=>{
+const products = await getCategories();
+console.log(products);
 let result =``;
-result+=categories.map((category)=>
+result+=products.map((product)=>
 `<tr>
-   <td>${category.name}</td>
-   <td><img src="${category.image.secure_url}"  width="50px"/></td>
-   <td><span class="badge badge-${category.status=='Active'?'success':'danger'}">${category.status}</span></td>
-   <td> <a href="#" onClick="deleteCategory('${category._id}',event)" class="btn btn-danger">
+   <td>${product.name}</td>
+   <td><img src="${product.image.secure_url}"  width="50px"/></td>
+   <td><span class="badge badge-${produc.status=='Active'?'success':'danger'}">${product.status}</span></td>
+   <td> <a href="#" onClick="deleteCategory('${product._id}',event)" class="btn btn-danger">
    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path fill="#fafcff" d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.7 23.7 0 0 0 -21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0 -16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"/></svg>
    </a>
   </tr>`
 )
-document.querySelector("#categories").innerHTML=result;
+document.querySelector("#products").innerHTML=result;
 }
 
 const deleteCategory = async (id,e)=>{
@@ -72,4 +72,4 @@ const deleteCategory = async (id,e)=>{
   }
  }
 
-displayCategory();
+displayProducts();
