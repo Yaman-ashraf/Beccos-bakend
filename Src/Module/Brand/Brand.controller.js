5151fdd0cc0d2c0eff75848548c3cb40d21f93a2
Yaman-ashraf <yaman472001@gmail.com>
2024-01-16 15:31:17 +0200

    grtBrand

--- Src/Module/Brand/Brand.controller.js
+++ Src/Module/Brand/Brand.controller.js
@@ -1,4 +1,5 @@
 import brandModel from "../../../DB/model/Brand.model.js";
+import sliderModel from "../../../DB/model/Slider.model.js";
 import cloudinary from "../../Services/cloudinary.js";
 
 export const createBrand = async (req, res) => {
@@ -8,10 +9,10 @@ export const createBrand = async (req, res) => {
     req.body.image = { secure_url, public_id };
     req.body.createdBy = req.user._id;
     req.body.updatedBy = req.user._id;
-    
+
     //create brand
     const brand = await brandModel.create(req.body);
-    
+
     return res.status(201).json({ message: "Success", brand });
 }
 
@@ -26,4 +27,12 @@ export const deleteBrand = async (req, res) => {
     if (!brands) return res.status(404).json({ message: "Brand not found" });
 
     return res.json({ message: "Success" });
+}
+
+export const getBrand = async (req, res) => {
+    const brand = await brandModel.findById(req.params.id);
+    if (!brand)
+        return res.status(404).json({ message: ' Brand not Found' });
+
+    return res.status(200).json({ message: "Success", brand });
 }
--- Src/Module/Brand/Brand.router.js
+++ Src/Module/Brand/Brand.router.js
@@ -5,8 +5,13 @@ import endPoint from './Brand.endpoint.js';
 import fileUpload, { fileValidation } from "../../Services/multer.js";
 
 const router = Router();
+
 router.post('/', auth(endPoint.create), fileUpload(fileValidation.image).single('image'), BrandController.createBrand);
 router.get('/', BrandController.getAllBrand);
 router.delete('/:brandId', auth(endPoint.delete), BrandController.deleteBrand);
+// router.get('/:brandId',BrandController.getProducts);
+router.get('/details/:id',BrandController.getBrand);
+
+
 
 export default router;
